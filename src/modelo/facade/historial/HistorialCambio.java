733de5f40af03df21cf5e930a0903cc37ea36911
Johan Sebastián Beltrán Merchán <sebas08beltran@gmail.com>
2025-02-08 17:30:15 -0500

    Clases de uso patrón facade

---
+++ src/modelo/facade/historial/HistorialCambio.java
@@ -0,0 +1,12 @@
+package modelo.facade.historial;
+
+import java.time.LocalDate;
+
+public class HistorialCambio {
+	private int idHistorial;
+	private int idUsuario;
+	private int idDocumento;
+	private String accion;
+	private LocalDate fechaModificacion;
+	
+}
---
+++ src/modelo/facade/reserva/Reserva.java
@@ -0,0 +1,9 @@
+package modelo.facade.reserva;
+
+public class Reserva {
+	private int idReserva;
+	private int idDocumento;
+	private int idUsuario;
+	private String estadoReserva;
+	
+}
