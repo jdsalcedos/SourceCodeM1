a76b707746ccfd85ba4d4e64588439a92831b18c
Juan Salcedo <jdsalcedodv@gmail.com>
2025-02-08 13:45:40 -0500

    Creacion clases DAO, falta implementar la logica

--- src/control/DAO/DocumentoDAO.java
+++ src/control/DAO/DocumentoDAO.java
@@ -1,8 +1,9 @@
 package control.DAO;
 
-public class UsuarioDAO implements InterfaceDAO {
+public class DocumentoDAO implements InterfaceDAO{
 
 	// toca reescribir todos los metodos para que funcionen con Usuarios
+	
 	@Override
 	public String getAll() {
 		// TODO Auto-generated method stub
@@ -40,3 +41,6 @@ public class UsuarioDAO implements InterfaceDAO {
 	}
 
 }
+
+
+
---
+++ src/modelo/usuario/Usuario.java
@@ -0,0 +1,82 @@
+package modelo.usuario;
+
+public class Usuario {
+	private int idUsuario;
+	private String nombre;
+	private String correo;
+	private String contrasena;
+	private String direccion;
+	private String telefono;
+	
+	
+	public Usuario(int idUsuario, String nombre, String correo, String contrasena, String direccion, String telefono) {
+		this.idUsuario = idUsuario;
+		this.nombre = nombre;
+		this.correo = correo;
+		this.contrasena = contrasena;
+		this.direccion = direccion;
+		this.telefono = telefono;
+	}
+
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getCorreo() {
+		return correo;
+	}
+
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	
+}
