1852e69d3a8aa682902732a90f335c846de501f0
SantiagoLopez <daviago1903@gmail.com>
2025-02-09 21:13:09 -0500

    Se agrego la función de login para autor y usuario

--- src/vista/VentanaRegistro.java
+++ src/vista/VentanaRegistro.java
@@ -214,6 +214,14 @@ public class VentanaRegistro extends JFrame {
 		contentPane.add(lblFondoRegistro);
 	}
 
+	public void clear(){
+		txtUsuario.setText("");
+		txtCorreo.setText("");
+		txtTelefono.setText("");
+		txtDireccion.setText("");
+		passwordRegistro.setText("");
+	}
+	
 	public boolean verify() {
 		String password = new String(passwordRegistro.getPassword()).trim();
 		if(txtUsuario.getText().trim().isEmpty()) {
